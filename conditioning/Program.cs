e4701aa02fe789a04eb0b01c3539394f5a955cb3
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 16:09:06 +0100

    done with condition

--- conditioning/Program.cs
+++ conditioning/Program.cs
@@ -11,6 +11,16 @@ namespace Conditioning
         static void Main(string[] args)
         {
             Console.WriteLine("Conditioning console app");
+            bool isMale = false;
+            bool isTall = true;
+
+            if(isMale && isTall)
+            {
+                Console.WriteLine("male");
+            } else
+            {
+                Console.WriteLine("female and short or you are one of the both");
+            }
         }
     }
 }
