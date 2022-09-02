b8b1c432fb47cdb9aa0984a611a1d9476f93a94c
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 15:19:12 +0100

    done with array in c#

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is an array console app");
+
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            string[] friends = new string[5];
+            friends[0] = "George";
+            friends[1] = "goldman";
+            Console.WriteLine(friends[0]+friends[1]);
+        }
+    }
+}
