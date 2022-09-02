d3961ba41b32d626fe40ea6c1fab58b6df618037
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 14:40:44 +0100

    completed the mad lib game

--- matlibgame/Program.cs
+++ matlibgame/Program.cs
@@ -10,7 +10,22 @@ namespace MatLibGame
     {
         static void Main(string[] args)
         {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("enter a plura noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
             Console.WriteLine("this is the mad lib game");
+            Console.WriteLine("Rose are "+color);
+            Console.WriteLine(pluralNoun+"  are blue");
+            Console.WriteLine("I love "+celebrity);
+            
         }
     }
 }
