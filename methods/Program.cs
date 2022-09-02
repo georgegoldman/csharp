8fbb3b9ccdf23ab39e466d61ea7de665c20a0d13
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 15:45:46 +0100

    done

---
+++ methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            SayHi("george");
+            SayHi("john");
+            SayHi("georgegoldman");
+            */
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+        }        
+        static void SayHi(string name)
+        {
+            Console.WriteLine("Hi "+name+"!");
+        }
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
