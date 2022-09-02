516d2eafb953b945adb9f1fe787e541bda620f8d
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 15:50:15 +0100

    mk condtioning app

---
+++ conditioning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditioning console app");
+        }
+    }
+}
