03531687153dfe3b243e61f7be52c87b42a58314
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-02 18:42:39 +0100

    done cal_v2

--- calculato_v2/Program.cs
+++ calculato_v2/Program.cs
@@ -11,6 +11,38 @@ namespace Calculator_v2
         static void Main(string[] args)
         {
             Console.WriteLine("version 2 calculator init");
+
+            Console.Write("enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("enter an operator: ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("enter a numbe: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == '+')
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == '-')
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == '/')
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == '*')
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+
         }
     }
 }
