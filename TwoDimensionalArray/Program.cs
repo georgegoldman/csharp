161eb32ee1d6bdf2d1e68162ac5182265281e72a
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-03 21:45:16 +0100

    init 2 d array

---
+++ TwoDimensionalArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is two dimensional array");
+
+            int[,] numberGrid = {
+                {1,2},
+                {3, 4},
+                {5, 6}
+            };
+
+            Console.WriteLine(numberGrid[1,1]);
+        }
+    }
+}
