639fbb0888bf623e5f5c9447a1fe978f172252ba
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-03 17:01:59 +0100

    init guessinggame

---
+++ guessinggame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "goldman";
+            string guess = "";
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            }
+            Console.WriteLine("you win!");
+        }
+    }
+}
