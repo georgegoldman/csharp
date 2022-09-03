2123cf8c210dcc1ddd0a71645ba32e9e5b2a59f9
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-03 21:10:07 +0100

    done

--- forloop/Program.cs
+++ forloop/Program.cs
@@ -10,7 +10,16 @@ namespace ForLoop
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(GetPow(2, 4));
+        }
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+            for (int i = 0; i < powNum; i++)
+            {
+             result = result * baseNum;   
+            }
+            return result;
         }
     }
 }
