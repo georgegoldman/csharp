1924a6be4eb776044517956ef70db4aaf3a9f40e
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-04 08:19:02 +0100

    done with exception_handling

--- exception_handling/Program.cs
+++ exception_handling/Program.cs
@@ -10,7 +10,30 @@ namespace Name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("exception handling");
+            Console.WriteLine("exception handling"); 
+
+            try
+            {
+                Console.Write("enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1/num2);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("please enter a valid input!");
+            }
+
         }
     }
 }
