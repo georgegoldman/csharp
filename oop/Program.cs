8a511d3a556169932cd258216c73a8ff8a1d1987
Georgegoldman John Onyedikachi <georgegoldmanjohn.o@gmail.com>
2022-09-04 12:06:53 +0100

    done

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c format: {0:c}", 99999);
+        }
+    }
+}
